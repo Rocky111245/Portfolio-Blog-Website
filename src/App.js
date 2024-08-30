44bc6f8b5f6c66aad5c6b98e9e780b180347fd3b
Rocky111245 <rakibalhasan170@gmail.com>
2024-08-30 16:58:33 +0800

    Changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./UI/Home/home";
 import Navbar from "./UI/Navbar/navbar";
 import AboutMe from './UI/About Me Page/AboutMe';
